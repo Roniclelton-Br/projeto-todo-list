070506ebdafc184d65e1304e40ee889c670efd48
Roniclelton Brito <ronicleltonbr@gmail.com>
2024-02-06 14:14:02 -0300

    criado funcionalidades dos botoes.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ const cancelEditBtn = document.querySelector('.cancel-edit');
 
 
 //FUNÇÕES
-
+//ADICIONANDO A TAREFA NA LISTA
 const saveTodo = (text) =>{
     const todo = document.createElement('div');
     todo.classList.add("list");
@@ -22,10 +22,49 @@ const saveTodo = (text) =>{
     todoTitle.innerHTML = text;
     todo.appendChild(todoTitle);
 
-    console.log(todo);
+    const icons = document.createElement('div'); 
+    icons.classList.add('icon');
+
+    const checkBtn = document.createElement('button');
+    checkBtn.classList.add('btn-check-todo');
+    checkBtn.innerHTML = '<i class="bi bi-check-lg"></i>';
+    icons.appendChild(checkBtn);
+
+    const editBtn = document.createElement('button');
+    editBtn.classList.add('btn-edite-todo');
+    editBtn.innerHTML = '<i class="bi bi-pen"></i>';
+    icons.appendChild(editBtn);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.classList.add('btn-eraser-todo');
+    deleteBtn.innerHTML = '<i class="bi bi-trash3"></i>';
+    icons.appendChild(deleteBtn);
+
+    todo.appendChild(icons);
+
+    todoList.appendChild(todo)
+
+    todoInput.value = '';
+    todoInput.focus();
+}
+// function heightTodoList(){
+//     if(todoList.children = 3) {
+//         todoList.style.height = 'auto';
+//         todoList.style.overflowY = 'auto';
+//     }else {
+//         todoList.style.height = '150px';
+//         todoList.style.overflowY= 'scroll';
+//     }
+
+// }
+// heightTodoList()
+
+function toggleInput(){
+    
 }
 
 //EVENTOS
+//EVENTO PARA QUANDO O INPUT ESTIVER VAZIO
 btnTodoAdd.addEventListener('click', (e) =>{
     e.preventDefault();
 
@@ -39,3 +78,24 @@ btnTodoAdd.addEventListener('click', (e) =>{
     
 
 });
+//EVENTO DE CANCELAR A TAREFA DO INPUT.
+cancalAddBtn.addEventListener('click', ()=>{
+    todoInput.value = '';
+});
+
+document.addEventListener('click', (e) => {
+    const targetEl = e.target;
+    const parentEl = targetEl.closest('.list');
+
+    if(targetEl.classList.contains('btn-check-todo')){
+        parentEl.classList.toggle('done')
+    }
+    if(targetEl.classList.contains('btn-eraser-todo')){
+        parentEl.remove();
+    }
+    if(targetEl.classList.contains('btn-edite-todo')) {
+        toggleInput();
+
+    }
+});
+
